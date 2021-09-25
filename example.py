7d91f099a35714ebebee6d241ef7c25eeae36e8e
Pablo Von Knoblauch Fontão <pablo-fontao@hotmail.com>
2021-09-25 16:00:10 -0300

    Conclusão do capítulo 2

--- example.py
+++ example.py
@@ -1,19 +1,24 @@
 def dividir(dividendo, divisor):
-    # print(divisor.resultado)
-    return dividendo/divisor
+    if not (isinstance(dividendo, int) and isinstance(divisor, int)):
+        raise ValueError("dividir() deve receber apenas argumentos inteiros")
+    try:
+        aux = dividendo/divisor
+        return aux
+    except ZeroDivisionError:
+        print(f"Não se pode dividir {dividendo} por {divisor}")
+        raise
 
 
 def testa_divisao(divisor):
-    try:
-        resultado = dividir(10, divisor)
-        print(f"O resultado da divisão de 10 por {divisor} é {resultado}")
-    except ZeroDivisionError:
-        print("Erro de divisão por 0 tratado")
+    resultado = dividir(10, divisor)
+    print(f"O resultado da divisão de 10 por {divisor} é {resultado}")
 
 
 try:
-    testa_divisao(0)
-except AttributeError:
-    print("Erro de atributo tratado")
+    testa_divisao('e5r')
+except ZeroDivisionError:
+    print("Erro de divisão por zero")
+# except Exception as erro_desconhecido:
+#     print("Erro desconhecido")
 
 print("Programa encerrado")
